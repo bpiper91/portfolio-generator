da03578756f0cb26cb6419172442bf8b99b77794
bpiper91 <bpiper91@gmail.com>
2022-07-26 22:44:25 -0400

    generates webpage and moves HTML template literal to separate file in src directory

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const profileDataArgs = process.argv.slice(2);
+// call file system module
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// take input data from the page template file
+const generatePage = require('./src/page-template.js');
 
-printProfileData(profileDataArgs);
+// capture the user input
+const profileDataArgs = process.argv.slice(2, process.argv.length); // items 0 & 1 are location and file being executed
+// get the name and GitHub username from the input text
+const [name, github] = profileDataArgs
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
